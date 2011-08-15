352b2cdfd276008f3e338042906b7e931a002c91
Roberto Scarciello <rscarciello@gmail.com>
2011-08-16 00:13:31 +0200

    Added RSOvalItem class

---
+++ RSReport/RSOvalItem.h
@@ -0,0 +1,16 @@
+//
+//  RSOvalItem.h
+//  RSReport
+//
+//  Created by Roberto Scarciello on 16/08/11.
+//  Copyright 2011 Roberto Scarciello. All rights reserved.
+//
+
+#import "RSGenericItem.h"
+
+@interface RSOvalItem : RSGenericItem
+
+@property (nonatomic, assign) BOOL fillRect;
+@property (nonatomic, assign) CGFloat lineWidth;
+
+@end
---
+++ RSReport/RSOvalItem.m
@@ -0,0 +1,36 @@
+//
+//  RSOvalItem.m
+//  RSReport
+//
+//  Created by Roberto Scarciello on 16/08/11.
+//  Copyright 2011 Roberto Scarciello. All rights reserved.
+//
+
+#import "RSOvalItem.h"
+
+@implementation RSOvalItem
+
+@synthesize fillRect = _fillRect;
+@synthesize lineWidth = _lineWidth;
+
+- (id)init
+{
+    self = [super init];
+    if (self) {
+        _lineWidth = 1.0;
+    }
+    
+    return self;
+}
+
+- (void)printItemInContext:(CGContextRef)context {
+    [super printItemInContext:context];
+    UIBezierPath *bp = [UIBezierPath bezierPathWithOvalInRect:self.frame];
+    bp.lineWidth = _lineWidth;
+    [bp stroke];
+    if (_fillRect) {
+        [bp fill];
+    }
+}
+
+@end
